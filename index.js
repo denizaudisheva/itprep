d3416a2c7a1a7b789dcd7a85aa1462e8b526cff3
denizaudisheva <denizaudisheva@gmail.com>
2025-02-16 21:31:29 +0500

    Обновление кода

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ function sendMail() {
         message: document.getElementById("message").value,
     });
 }
+function sendMail() {
+    emailjs
+        .send("service_g08pqfc", "template_2dlf4zw", {
+            name: document.getElementById("name").value,
+            message: document.getElementById("message").value,
+        })
+        .then(
+            function (response) {
+                // Показываем сообщение об успешной отправке
+                document.getElementById("successMessage").style.display =
+                    "block";
+
+                // Очищаем форму
+                document.getElementById("name").value = "";
+                document.getElementById("message").value = "";
+
+                // Через 3 секунды убираем сообщение
+                setTimeout(() => {
+                    document.getElementById("successMessage").style.display =
+                        "none";
+                }, 3000);
+            },
+            function (error) {
+                alert("Ошибка при отправке. Попробуйте снова!");
+            }
+        );
+}
